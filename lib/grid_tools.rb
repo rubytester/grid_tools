0280b7efb604a8d695bad34ac3801268e08350b9
Alan Scherger <ascherger@homeaway.com>
2012-11-29 17:57:10 -0600

    added in basic node logic

--- lib/grid_tools.rb
+++ lib/grid_tools.rb
@@ -2,3 +2,20 @@ require 'grid_tools/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+
+module GridTools
+
+ def self.windows?
+   (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+ end
+ def self.mac?
+   (/darwin/ =~ RUBY_PLATFORM) != nil
+ end
+ def self.unix?
+   !OS.windows?
+ end
+ def self.linux?
+   OS.unix? and not OS.mac?
+ end
+
+end
